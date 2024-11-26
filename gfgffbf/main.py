29f63e991755971d025caf8404eb0e7be0fe790c
Lpopivnyak <stadnykroman2010@gmail.com>
2024-11-26 17:21:43 +0200

    ttgftrgtrgtrgtrgtgfthyhy

--- gfgffbf/main.py
+++ gfgffbf/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('train.csv')
+df = pd.read_csv('../train.csv')
 
 df.info()
 df.drop(['PassengerId','Name','Ticket','Cabin'],axis = 1 ,inplace = True)
---
+++ main1.py
@@ -0,0 +1,18 @@
+#створи тут свій індивідуальний проект!
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report,confusion_matrix
+import joblib
+
+df = pd.read_csv('train.csv')
+
+df['bdate'] = df['bdate'].fillna('01.0.1900')
+df['bdate'] = pd.to_datetime(df['bdate'], format = '%d.%m.%Y', errors ='coerce')
+
+current_year = pd.to_datetime('today').year
+df['age'] = current_year = df['bdate'].dt.year
+df['age'] = df['age'].fillna(df['age'].median())
+df.drop(colums=['bdate'], inplace=True)
